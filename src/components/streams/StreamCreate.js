38c74f0afb01a1f753a812c2513d7e2a515f0dc9
Richard Levey <riclev123@gmail.com>
2019-02-24 22:03:20 +0200

    Error validation using redux form on stream creation.

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,21 +2,57 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+    renderInput({input, label}) {
+        return (
+            <div className={"field"}>
+            <label>
+                {label}
+            </label>
+                <input 
+                    {...input} 
+                />
+            </div>
+        );
+    }
+
+    onSubmit (formValues) {
+        console.log(formValues)
+    }
+
     render() {
         return (
-            <form>
+            <form className={"ui form"} onSubmit={this.props.handleSubmit(this.onSubmit)}>
                 <Field
                     name={"title"}
-                    component={}
+                    label={"Enter Title"}
+                    component={this.renderInput}
                 />
                 <Field
                     name={"description"}
-                    component={}
+                    label={"Enter Description"}
+                    component={this.renderInput}
                 />
+                <button className={"ui button primary"}>
+                    Submit
+                </button>
             </form>
         )
     }
 }
+
+const validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'You must enter a title';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+
+    return errors;
+};
 
 export default reduxForm({
     form: 'streamCreate'
