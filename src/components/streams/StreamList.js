83d212380bf0477ec1fd36924157b0af6a82fba2
Richard Levey <richard@2waysystems.com>
2019-02-26 18:00:15 +0200

    Added lodash library for array and object management Added axios library for http request Integrated redux thunk middleware for async actions Set up connection to stream api Set up reducer/actions for stream api RESTful integration Connected stream list component to redux with near intention to display a list of active streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-    return (
-        <div>
-            Stream List
-        </div>
-    )
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    render() {
+        return (
+            <div>
+                Stream List
+            </div>
+        );
+    }
 };
 
-export default StreamList;
+export default connect(null, {
+    fetchStreams
+})(StreamList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 import { reducer as formReducer } from "redux-form";
 import { combineReducers } from 'redux';
 
 export default combineReducers({
     auth: authReducer,
-    form:  formReducer
+    form:  formReducer,
+    stream: streamReducer
 });
