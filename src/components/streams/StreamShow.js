8277c47cc42e4b8716198464731fcd8d06641863
Richard <richard.levey@mtzl.net>
2019-03-19 07:17:08 +0200

    Streaming functional and working as expected through flv

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,45 @@
 import React from 'react';
 
+import flv from 'flv.js';
+
 import {connect} from 'react-redux';
 import {fetchStream} from "../../actions";
 
 class StreamShow extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+        const { id } = this.props.match.params;
+        this.props.fetchStream(id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer() {
+        if(this.player || !this.props.stream) {
+            return;
+        }
+
+        const { id } = this.props.match.params;
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render() {
@@ -17,6 +51,13 @@ class StreamShow extends React.Component {
 
         return (
             <div>
+                <video
+                    style={{
+                        width: "100%"
+                    }}
+                    controls
+                    ref={this.videoRef}
+                />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
@@ -30,6 +71,6 @@ const mapStateToProps = (state, ownProps) => {
     }
 };
 
-export default connect(mapStateToProps(), {
+export default connect(mapStateToProps, {
     fetchStream
 })(StreamShow);
